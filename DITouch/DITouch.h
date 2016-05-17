6f6875dcb96aedc5831103cfee028fa1e944779a
BackYu <szyhf@qq.com>
2016-05-17 11:56:52 +0800

    Initial Commit

---
+++ DITouch/DITouch.h
@@ -0,0 +1,19 @@
+//
+//  DITouch.h
+//  DITouch
+//
+//  Created by Back on 16/5/17.
+//  Copyright © 2016年 Back. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DITouch.
+FOUNDATION_EXPORT double DITouchVersionNumber;
+
+//! Project version string for DITouch.
+FOUNDATION_EXPORT const unsigned char DITouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DITouch/PublicHeader.h>
+
+
